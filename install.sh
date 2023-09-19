8eab2a3ad6f74335c32c76247a9b829311bc15e7
Seokmin Hong <hsmint.hong@gmail.com>
2023-09-19 14:28:24 +0900

    [INIT] initial commit

---
+++ install.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# Define variables
+GREEN="$(tput setaf 2)[OK]$(tput sgr0)"
+RED="$(tput setaf 1)[ERROR]$(tput sgr0)"
+YELLOW="$(tput setaf 3)[NOTE]$(tput sgr0)"
+CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
+LOG="install.log"
+
+# Set the script to exit on error
+set -e
+
+printf "$(tput setaf 2) Welcome to the Arch Linux YAY Hyprland installer!\n $(tput sgr0)"
+
+sleep 2
+
+printf "$YELLOW PLEASE BACKUP YOUR FILES BEFORE PROCEEDING!
+This script will overwrite some of your configs and files!"
+
+sleep 2
+
+printf "\n
+$YELLOW  Some commands requires you to enter your password inorder to execute
+If you are worried about entering your password, you can cancel the script now with CTRL Q or CTRL C and review contents of this script. \n"
+
+sleep 3
+
+### Disable wifi powersave mode ###
+read -n1 -rep 'Would you like to disable wifi powersave? (y,n)' WIFI
+if [[ $WIFI == "Y" || $WIFI == "y" ]]; then
+    LOC="/etc/NetworkManager/conf.d/wifi-powersave.conf"
+    echo -e "The following has been added to $LOC.\n"
+    echo -e "[connection]\nwifi.powersave = 2" | sudo tee -a $LOC
+    echo -e "\n"
+    echo -e "Restarting NetworkManager service...\n"
+    sudo systemctl restart NetworkManager
+    sleep 3
+fi
+
+#Check yay is installed
+ISyay=/sbin/yay
+
+if [ -f "$ISyay" ]; then
+    printf "\n%s - yay was located, moving on.\n" "$GREEN"
+else
+    printf "\n%s - yay was NOT located\n" "$YELLOW"
+    read -n1 -rep "${CAT} Would you like to install yay (y,n)" INST
+    if [[ $INST =~ ^[Yy]$ ]]; then
+        git clone https://aur.archlinux.org/yay.git
+        cd yay
+        makepkg -si --noconfirm 2>&1 | tee -a $LOG
+        cd ..
+    else
+        printf "%s - yay is required for this script, now exiting\n" "$RED"
+        exit
+    fi
+# update system before proceed
+    printf "${YELLOW} System Update to avoid issue\n"
+    yay -Syu --noconfirm 2>&1 | tee -a $LOG
+fi
+
+# Function to print error messages
+print_error() {
+    printf " %s%s\n" "$RED" "$1" "$NC" >&2
+}
+
+# Function to print success messages
+print_success() {
+    printf "%s%s%s\n" "$GREEN" "$1" "$NC"
+}
+
+### Install packages ####
+read -n1 -rep "${CAT} Would you like to install the packages? (y/n)" inst
+echo
+
+if [[ $inst =~ ^[Nn]$ ]]; then
+    printf "${YELLOW} No packages installed. Goodbye! \n"
+            exit 1
+        fi
+
+if [[ $inst =~ ^[Yy]$ ]]; then
+    hypr_pkgs="hyprland-git wl-clipboard wf-recorder rofi wlogout swaylock-effects dunst swaybg kitty waybar-hyprland-git"
+    font_pkgs="ttf-jetbrains-mono-nerd ttf-icomoon-feather noto-fonts-emoji ttf-font-awesome"
+    audio_pkgs="pulseaudio pulseaudio-alsa pamixer playerctl"
+    app_pkgs="nwg-look qt5ct ffmpegthumbs swww btop mousepad"
+    bluetooth_pkgs="bluez bluez-utils"
+    laptops_pkgs="brightnessctl"
+    file_managers_pkgs="thunar lxappearance xfce4-settings xdg-desktop-portal-hyprland"
+    theme_pkgs="rofi-emoji dracula-gtk-theme dracula-icons-git nordic-theme papirus-icon-theme starship"
+
+    if ! yay -S --noconfirm $hypr_pkgs $font_pkgs $audio_pkgs $app_pkgs $bluetooth_pkgs $laptops_pkgs $file_managers_pkgs $theme_pkgs 2>&1 | tee -a $LOG; then
+        print_error " Failed to install additional packages - please check the install.log \n"
+        exit 1
+    fi
+    xdg-user-dirs-update
+    echo
+    print_success " All necessary packages installed successfully."
+else
+    echo
+    print_error " Packages not installed - please check the install.log"
+    sleep 1
+fi
+
+# BLUETOOTH
+read -n1 -rep "${CAT} OPTIONAL - Would you like to install Bluetooth packages? (y/n)" BLUETOOTH
+if [[ $BLUETOOTH =~ ^[Yy]$ ]]; then
+    printf " Installing Bluetooth Packages...\n"
+ blue_pkgs="bluez bluez-utils blueman"
+    if ! yay -S --noconfirm $blue_pkgs 2>&1 | tee -a $LOG; then
+       	print_error "Failed to install bluetooth packages - please check the install.log"
+    printf " Activating Bluetooth Services...\n"
+    sudo systemctl enable --now bluetooth.service
+    sleep 2
+    fi
+else
+    printf "${YELLOW} No bluetooth packages installed..\n"
+	fi
+
+### Script is done ###
+printf "\n${GREEN} Installation Completed.\n"
